66894b9de5aaacb8b8471a772e200f092f8f0fca
ChrisAchkar <cachkar0408@gmail.com>
2024-09-28 22:55:46 -0400

    edited back_end

--- back_end.py
+++ back_end.py
@@ -17,10 +17,18 @@ Steps to get API key:
 Now you should be able to run your code in the console normally.    
 '''
 
+# configure model
 genai.configure(api_key=os.environ["API_KEY"])
 model = genai.GenerativeModel("gemini-1.5-flash")
 
 # get student info from HTML
+"""
+add code here
+add code here
+add code here
+add code here
+add code here
+"""
 # sample info below
 courses = {"DSA 1"}
 name = "Chris"
@@ -35,15 +43,15 @@ def read_file(file):
         sql_script = file.read()
     return sql_script
 
-# read in relevant files
-all_students = read_file("SQLTables/students.sql") # SQL database of students
-all_courses = read_file("SQLTables/courses.sql") # all courses
-student_courses = read_file("SQLTables/student_courses.sql") # courses taking
+# read in files with student info as Strings
+all_students = read_file("SQLTables/students.sql")
+all_courses = read_file("SQLTables/courses.sql")
+student_courses = read_file("SQLTables/student_courses.sql")
 all_interests = read_file("SQLTables/interests.sql")
 student_interests = read_file("SQLTables/student_interests.sql")
 
 # create specific prompt with given info for Gemini
-prompt = ("\nLooking at these SQL files, which students would work best with "
+prompt = ("\nLooking at these SQL files, which 3 students would work best with "
          "the given student?\n"
          f"File of all students: \n{all_students}\n" 
          f"File of all courses: \n{all_courses}\n" 
@@ -61,4 +69,10 @@ response = model.generate_content(prompt)
 print(response.text)
 
 # send output to HTML
-# code here...
+"""
+add code here
+add code here
+add code here
+add code here
+add code here
+"""
